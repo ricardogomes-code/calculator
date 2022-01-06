e207e44d8ad8c648a3208b124cfc656889d69a73
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-01-06 16:02:53 +0000

    C04 - Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,6 +3,18 @@ class CalcController{
     constructor(){
         this._currentDate;
         this._displayCalc;
+
+        this.initialize();
+    }
+
+    initialize(){
+        let displayEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#date");
+        let timeEl = document.querySelector("#time");
+
+        displayEl.innerHTML = "237";
+        dateEl.innerHTML = "01/01/2021";
+        timeEl.innerHTML = "14:01";
     }
 
     get currentDate(){
