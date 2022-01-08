ac87c07fe6e1baf1eb668262d4558d84cec9ad98
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-01-08 18:14:40 +0000

    C08 - Aplicando vários Eventos usando split()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -13,15 +13,28 @@ class CalcController {
         this.initButtons();
     }
 
+    addEventListenerAll(btn, events, fn){
+
+        events.split(" ").forEach(event => {
+            btn.addEventListener(event, fn, false);
+        });
+    }
+
     initButtons(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
         
-        buttons.forEach(btn=>{
-            btn.addEventListener("click", e=>{
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, "click drag", e => {
                 console.log(btn.className.baseVal.replace("btn-", ""));
             })
-        })
+        });
+
+        buttons.forEach(btn => {
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+                btn.style.cursor = "pointer";
+            });
+        });
     }
     
     initialize() {
